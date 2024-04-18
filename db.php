92b3fab1e68590f8e4f4b214beeff18552a7f7ff
StefanRaluca <raluca.bubulina@yahoo.com>
2024-04-18 17:21:15 +0200

    add bonus 2 with genre accepted in production.php

--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php
 
-
 require_once __DIR__ . "./models/production.php";
 require_once __DIR__ . './models/genre.php';
 
+$action = new Genre("Action", "Movies with action scenes,behavior of the program is checked against typical malware action patterns: Type of action and action sequences.");
+$comedy = new Genre("Comedy", "Comedy entertainment, particular scenes in them, that are intended to make people laugh.");
+$thriller = new Genre("Drama", "Thriller film, that tells an exciting fictional story about something such as criminal activities or spying.");
+
 $productions = [
-    new Production("Matrix", "Inglese", 9),
-    new Production("Done", "Italiano", 8),
-    new Production("Game of Throns", "Francese", 7),
-    new Production("Cold War", "Inglese", 10),
-    new Production(" OldBoy", "Italiano", 5),
+    new Production("Matrix", "Inglese", 9, $action),
+    new Production("Done", "Italiano", 8, $action),
+    new Production("Game of Throns", "Francese", 7, $thriller),
+    new Production("Cold War", "Inglese", 10, $thriller),
+    new Production(" OldBoy", "Italiano", 5, $comedy),
 ];
--- models/production.php
+++ models/production.php
@@ -6,11 +6,12 @@ class Production
     public $title;
     public $language;
     public $vote;
-
-    public function __construct($title, $language, $vote)
+    public $genre;
+    public function __construct($title, $language, $vote, $genre, )
     {
         $this->title = $title;
         $this->language = $language;
         $this->vote = $vote;
+        $this->genre = $genre;
     }
 }
